7f23e2b365b6ef0e2e44dd9c7c737cece7866454
Joshua Legere <joshualegere@Joshuas-MacBook-Pro.local>
2025-12-04 14:12:11 -0400

    Clean static site

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,html}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Inter", "sans-serif"]
+      }
+    },
+  },
+  plugins: [],
+};
+
+
+
+
+
+
+
+
